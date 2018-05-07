Fixed errors with averaging and applied momentum to line finding

Line.py
@@ -58,8 +58,8 @@ class Line():
 
         if (mode=='recent'):
             # Calculate the new radii of curvature of left and right lines in pixel dimensions
-            self.right_curv = ((1 + (2*self.right_poly [0]*y_eval + self.right_poly [1])**2)**1.5) / np.absolute(2*self.right_poly[0])
-            self.left_curv = ((1 + (2*self.left_poly [0]*y_eval + self.left_poly [1])**2)**1.5) / np.absolute(2*self.left_poly[0])
+            self.right_curv = ((1 + (2*self.right_poly[0]*y_eval + self.right_poly[1])**2)**1.5) / np.absolute(2*self.right_poly[0])
+            self.left_curv = ((1 + (2*self.left_poly[0]*y_eval + self.left_poly[1])**2)**1.5) / np.absolute(2*self.left_poly[0])
         elif (mode=='avg'):
             # Calculate the new radii of curvature of left and right lines in pixel dimesions
             self.avg_right_curv = ((1 + (2*self.avg_right_poly[0]*y_eval + self.avg_right_poly[1])**2)**1.5) / np.absolute(2*self.avg_right_poly[0])
@@ -70,7 +70,7 @@ class Line():
             self.radius_of_curvature = (self.avg_right_curv + self.avg_left_curv) / 2
 
             #for straight lines radius of curvs would be very high
-            if(self.radius_of_curvature > 8000):
+            if(self.radius_of_curvature > 10000):
                 self.radius_of_curvature = np.inf
 
          
@@ -82,7 +82,7 @@ class Line():
             self.valid_new = True
         else:
             #comparing left and right curvs
-            if ((self.right_curv > 4000) & (self.right_curv > 4000)):
+            if ((self.right_curv > 3000) & (self.right_curv > 3000)):
                 #straigh line case
                 curv_error = 0
             else:
@@ -90,27 +90,34 @@ class Line():
         
             #comparing to previous values
             #defining error values
-            right_curv_error = 0
-            left_curv_error = 0
-            right_curv_error = abs( (self.avg_right_curv - self.right_curv) / self.avg_right_curv )
-            left_curv_error = abs( (self.avg_left_curv - self.left_curv) / self.avg_left_curv )
+            if(self.avg_right_curv > 3000):
+                right_curv_error = 0
+            else:
+                right_curv_error = abs( (self.avg_right_curv - self.right_curv) / self.avg_right_curv )
+            if(self.avg_left_curv > 3000):
+                left_curv_error = 0
+            else:
+                left_curv_error = abs( (self.avg_left_curv - self.left_curv) / self.avg_left_curv )
 
             #position error
             y_eval = self.dim[0]
-            leftx_base = self.left_poly[0]*y_eval**2 + self.left_poly[1]*y_eval * self.left_poly[2]
-            rightx_base = self.right_poly[0]*y_eval**2 + self.right_poly[1]*y_eval * self.right_poly[2]
+            leftx_base = self.left_poly[0]*y_eval**2 + self.left_poly[1]*y_eval + self.left_poly[2]
+            rightx_base = self.right_poly[0]*y_eval**2 + self.right_poly[1]*y_eval + self.right_poly[2]
             base_diff_pix = rightx_base - leftx_base
             base_diff_act = self.xm_per_pix * base_diff_pix
             base_error = abs(base_diff_act - 3.7) / 3.7
 
             #appending new values
-            if ((right_curv_error < 0.1) & (left_curv_error < 0.1) & (curv_error < 0.1) & (base_error < 0.1)):
+            if ((right_curv_error < 0.5) & (left_curv_error < 0.5) & (curv_error < 0.8) & (base_error < 0.1)):
                 self.valid_new = True
+                self.last_valid_frame=0
             else: 
                 self.valid_new = False
                 self.last_valid_frame += 1
-                if(self.last_valid_frame>=25):
+                if(self.last_valid_frame>=10):
                     self.detected = False
+                    self.valid_new = True
+                    self.last_valid_frame=0
         
     def cvrt_2_act(self):
         #convert polynomials from pixel to actual dimensions in meter
@@ -119,26 +126,24 @@ class Line():
 
     def find_avg(self):
         if (self.detected):
-            self.avg_right_poly = np.add(np.multiply(0.9, self.avg_right_poly), np.multiply(0.1, self.right_poly ) )
-            self.avg_left_poly = np.add(np.multiply(0.9, self.avg_left_poly), np.multiply(0.1, self.left_poly ) )
+            self.avg_right_poly = np.add(np.multiply(0.8, self.avg_right_poly), np.multiply(0.2, self.right_poly ) )
+            self.avg_left_poly = np.add(np.multiply(0.8, self.avg_left_poly), np.multiply(0.2, self.left_poly ) )
         else:
             self.avg_right_poly = self.right_poly
             self.avg_left_poly = self.left_poly
             self.detected = True
-            print('not ok')
         
         self.cvrt_2_act()
 
     def fit_poly(self):
         # Fit a second order polynomial to each
-        self.right_poly = np.polyfit(self.lefty, self.leftx, 2)
-        self.left_poly = np.polyfit(self.righty, self.rightx, 2)
+        self.left_poly = np.polyfit(self.lefty, self.leftx, 2)
+        self.right_poly = np.polyfit(self.righty, self.rightx, 2)
 
         self.sanity_check()
         if(self.valid_new):
             self.find_avg()
             self.find_curvature(mode='avg')
-            self.last_valid_frame = 0
 
     def add_points(self, lefty, leftx, righty, rightx):
         self.leftx = leftx

code.py
@@ -170,14 +170,14 @@ def find_lines(binary_warped, Line):
     if (slide_mode == 'regular'):
         # Find the peak of the left and right halves of the histogram
         # These will be the starting point for the left and right lines
+        midpoint = np.int(histogram.shape[0]//2)
         if (Line.detected==False):
-            midpoint = np.int(histogram.shape[0]//2)
             leftx_base = np.argmax(histogram[masking_x_region:midpoint]) + masking_x_region
             rightx_base = np.argmax(histogram[midpoint:(histogram.shape[0]-masking_x_region)]) + midpoint
         else:
             y_eval = binary_warped.shape[0]
-            leftx_base = int(Line.avg_left_poly[0]*y_eval**2 + Line.avg_left_poly[1]*y_eval * Line.avg_left_poly[2])
-            rightx_base = int(Line.avg_right_poly[0]*y_eval**2 + Line.avg_right_poly[1]*y_eval * Line.avg_right_poly[2])
+            leftx_base = int(Line.avg_left_poly[0]*y_eval**2 + Line.avg_left_poly[1]*y_eval + Line.avg_left_poly[2])
+            rightx_base = int(Line.avg_right_poly[0]*y_eval**2 + Line.avg_right_poly[1]*y_eval + Line.avg_right_poly[2])
 
         # Current positions to be updated for each window
         leftx_current = leftx_base
@@ -188,6 +188,10 @@ def find_lines(binary_warped, Line):
         # Create empty lists to receive left and right lane pixel indices
         left_lane_inds = []
         right_lane_inds = []
+
+        #momentum that helps the lines keep moving in the same direction
+        right_momentum = 0
+        left_momentum = 0
 
         # Step through the windows one by one
         for window in range(nwindows):
@@ -211,9 +215,20 @@ def find_lines(binary_warped, Line):
             right_lane_inds.append(good_right_inds)
             # If you found > minpix pixels, recenter next window on their mean position
             if len(good_left_inds) > minpix:
-                leftx_current = np.int(np.mean(nonzerox[good_left_inds]))
+                left_change = np.int(np.mean(nonzerox[good_left_inds])) - leftx_current
+                if(left_momentum==0):
+                    left_momentum = left_change
+                else:
+                    left_momentum = 0.2*left_momentum + 0.8*left_change
             if len(good_right_inds) > minpix:        
-                rightx_current = np.int(np.mean(nonzerox[good_right_inds]))
+                right_change = np.int(np.mean(nonzerox[good_right_inds])) - rightx_current
+                if(right_momentum!=0):
+                    right_momentum = right_change
+                else:
+                    right_momentum = 0.2*right_momentum + 0.8*right_change
+
+            leftx_current = leftx_current + int(left_momentum)        
+            rightx_current = rightx_current + int(right_momentum)
 
         # Concatenate the arrays of indices
         left_lane_inds = np.concatenate(left_lane_inds)
@@ -337,34 +352,34 @@ def process_img(input_img):
     #perform color and sobel thresholding
     thresh_image, color_binary = threshold(input_img, color_s_thresh, color_l_thresh, sobel_dir_thresh, sobel_mag_thresh)  
   
-    ##apply perspective transform
-    #per_img = perspective_transform(thresh_image, source_points, destination_points)
+    #apply perspective transform
+    per_img = perspective_transform(thresh_image, source_points, destination_points)
     
-    ##set parameters for myline object
-    #myLine.set_param(input_img.shape, 3/110, 3.7/680)
+    #set parameters for myline object
+    myLine.set_param(input_img.shape, 3/110, 3.7/680)
 
-    ##finding and fitting lane lines
-    #line_finding_img = find_lines(per_img, myLine)
+    #finding and fitting lane lines
+    line_finding_img = find_lines(per_img, myLine)
     
     
-    #if (myLine.detected):
-    #    #marking lane lines
-    #    wraped_marked_img = plot(per_img, myLine)
+    if (myLine.detected):
+        #marking lane lines
+        wraped_marked_img = plot(per_img, myLine)
 
-    #    #applying inverse perspective transform
-    #    marked_img = perspective_transform(wraped_marked_img, destination_points, source_points)
+        #applying inverse perspective transform
+        marked_img = perspective_transform(wraped_marked_img, destination_points, source_points)
 
-    #    #adding marked image to original image
-    #    added_img = cv2.addWeighted(input_img, 1, marked_img, 0.5, 0)
+        #adding marked image to original image
+        added_img = cv2.addWeighted(input_img, 1, marked_img, 0.5, 0)
 
-    #    #annotate image
-    #    annotate_img = cv2.putText(added_img,"Line curveture: {0:.2f} km".format(myLine.radius_of_curvature/1000), (100,100), cv2.FONT_HERSHEY_COMPLEX, 1, 255)
+        #annotate image
+        annotate_img = cv2.putText(added_img,"Line curveture: {0:.2f} km".format(myLine.radius_of_curvature/1000), (100,100), cv2.FONT_HERSHEY_COMPLEX, 1, 255)
 
-    #else:
-    #    annotate_img = np.copy(input_img)
+    else:
+        annotate_img = np.copy(input_img)
 
     #Adding marked lines to original images
-    return thresh_image
+    return annotate_img
 
 
 def main():
@@ -410,7 +425,7 @@ def main():
         plt.show()
 
     elif (mode == 'video'):
-        input_clip = VideoFileClip(video_dir).subclip(20,21)
+        input_clip = VideoFileClip(video_dir)
         output_clip = input_clip.fl_image(process_img)
         output_clip.write_videofile(video_dir_out, audio=False)
 